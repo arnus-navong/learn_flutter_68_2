4d771a47712ef15ac39f9864fc7f1e8953d5c281
Arnus <nut00ped.navong@gmail.com>
2026-06-13 11:24:22 +0700

    Step 7: Button widget

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -6,18 +6,65 @@ class Home extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: [
-        Image.asset("assets/images/tung.jpg", width: 200, height: 200),
-        SizedBox(height: 20),
-        Image.network(
-          "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
-          width: 200,
-          height: 200,
-        ), // Image.network
-      ]   
-    );  // Column
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          TextButton(
+            style: TextButton.styleFrom(foregroundColor: Colors.pinkAccent),
+            onPressed: () {
+              print("กดปุ่มแล้ว");
+            },
+            child: Text(
+              "กดปุ่มนี้",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ), 
+          ),
+          const SizedBox(height: 20), // เพิ่มระยะห่างระหว่างปุ่ม
+          FilledButton(
+            style: FilledButton.styleFrom(
+              backgroundColor: Colors.purpleAccent,
+              foregroundColor: Colors.white,
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            ),
+            onPressed: () {
+              print("กดปุ่มแล้ว FilledButton");
+            },
+            child: Text(
+              "กดปุ่มนี้",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ), 
+          ),
+          const SizedBox(height: 20), // เพิ่มระยะห่างระหว่างปุ่ม
+          OutlinedButton(
+            style: OutlinedButton.styleFrom(
+              side: BorderSide(color: Colors.blueAccent, width: 2),
+              foregroundColor: Colors.blueAccent,
+            ),
+            onPressed: () {
+              print("กดปุ่มแล้ว OutlinedButton");
+            },
+            child: Text(
+              "กดปุ่มนี้",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          const SizedBox(height: 20), // เพิ่มระยะห่างระหว่างปุ่ม
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.yellowAccent,
+              foregroundColor: Colors.white,
+            ),
+            onPressed: () {
+              print("กดปุ่มแล้ว ElevatedButton");
+            },
+            child: Text(
+              "กดปุ่มนี้",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ), 
+          )
+        ],
+      ) ,// Column
+    ); // Center
   }
 }
