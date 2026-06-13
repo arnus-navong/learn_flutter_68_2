6de4dec8832c78b2226c199420cc46228bb66b6b
Arnus <nut00ped.navong@gmail.com>
2026-06-13 11:54:16 +0700

    Step 8: Stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -2,20 +2,22 @@ import 'package:flutter/material.dart';
 
 import 'package:learn_flutter_68_2/screen/home.dart';
 
+import 'package:learn_flutter_68_2/screen/item.dart';
+
 void main() {
   //const app = MaterialApp(title: 'หัวข้อ', home: Text(("Hello World")));
   //runApp(app);
 
   runApp(
     MaterialApp(
-      title: "My App",
+      title: "หัวข้อ",
       home: Scaffold(
         appBar: AppBar(
-          title: const Text("Home"),
+          title: const Text("หัวข้อ"),
           backgroundColor: Colors.pinkAccent,
           centerTitle: true,
         ), // AppBar
-        body: Home(),
+        body: Item(),
       ), // Scaffold
     ), // MaterialApp
   );
