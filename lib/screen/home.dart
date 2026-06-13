22d196971d6b9fbe7fbd16291fc4c09b93f7d44c
Arnus <nut00ped.navong@gmail.com>
2026-06-13 10:48:27 +0700

    Step 6: Images widget

--- lib/screen/home.dart
+++ lib/screen/home.dart
@@ -7,26 +7,17 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      // mainAxisAlignment: MainAxisAlignment.start,
-      // mainAxisAlignment: MainAxisAlignment.center,
-      // mainAxisAlignment: MainAxisAlignment.end,
-      // mainAxisAlignment: MainAxisAlignment.spaceAround,
+      mainAxisAlignment: MainAxisAlignment.center,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        Container( 
-          color: Colors.purpleAccent,
-          padding: EdgeInsets.symmetric(horizontal: 5, vertical: 5),
-          child: Text(
-            "Hello Flutter5",
-            style: TextStyle(
-              fontSize: 20,
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-              letterSpacing: 3,
-            ),
-          ),
-    )
-      ],
-    ); 
+        Image.asset("assets/images/tung.jpg", width: 200, height: 200),
+        SizedBox(height: 20),
+        Image.network(
+          "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
+          width: 200,
+          height: 200,
+        ), // Image.network
+      ]   
+    );  // Column
   }
 }
